07e95bb7e732c79585a2cddd496b900ba054abe7
Vesa Karvonen <vesa.a.j.k@gmail.com>
2019-05-09 23:22:09 +0300

    Highlight Bash snippets

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const marked = require('marked')
 const sourcePath = 'README.md'
 const targetPath = 'index.html'
 
+const hljsStyle = 'googlecode'
+const hljsVersion = '9.12.0'
+
 marked.setOptions({
   renderer: new marked.Renderer(),
   gfm: true,
@@ -26,17 +29,22 @@ fs.readFileSync(sourcePath)
   .pipe(s => marked(String(s)))
   .replace(/ id="a-[^"]*"/g, '')
   .replace(/ +$/gm, '')
+  .replace(/<code class="language-([a-z]*)">/g, '<code class="hljs lang-$1">')
   .pipe(
     s => `<!DOCTYPE html>
 <html lang="en">
 <head>
-<link rel="stylesheet" href="github.css">
-<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" type="text/css" href="github.css">
+<link rel="stylesheet" type="text/css" href="styles.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/${hljsVersion}/styles/${hljsStyle}.min.css">
 <meta charset="utf-8">
 <title>C++ submodule manager</title>
 </head>
 <body class="markdown-body">
 ${s}
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/${hljsVersion}/highlight.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/${hljsVersion}/languages/bash.min.js"></script>
+<script type="text/javascript" src="init-hljs.js"></script>
 </body>
 </html>`
   )
